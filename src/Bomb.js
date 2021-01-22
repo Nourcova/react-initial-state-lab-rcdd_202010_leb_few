64b9c30686d2101ed26567d9072cf4b6fce98a18
Ibrahim Nour <ibrahamnour22@gmail.com>
2021-01-22 02:52:05 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,9 +1,22 @@
-class Bomb extends Component {
-   constructor(props) {
-     super(props);
-     this.state = {  }
-   }
-   render() { 
-     return (  );
-   }
- }
+import React from 'react';
+
+class Bomb extends React.Component {
+
+  constructor(props) {
+    super() 
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return (
+      <div>{message}</div>
+    )
+  }
+}
+
+export default Bomb;
